500e3281c2fbc09a53b149c411e229757616d313
chinari <23wh1a0520@bvrithyderabad.edu.in>
2026-01-03 14:13:13 +0530

    fix backend

--- backend/src/routes/shortURLRouter.js
+++ backend/src/routes/shortURLRouter.js
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import { protect } from "../middlewares/authMiddleware.js";
-import { redirectFunction, shortUrl, getUserUrls } from "../controllers/shortUrlController.js";
+import {
+  redirectFunction,
+  shortUrl,
+  getUserUrls,
+} from "../controllers/shortUrlController.js";
 
 const shortURLRouter = Router();
 
-shortURLRouter.post("", protect, shortUrl);
-shortURLRouter.get("/:shortcode", redirectFunction);
-// GET /api/s/history - get all URLs for the logged-in user
+/* CREATE SHORT URL */
+shortURLRouter.post("/", protect, shortUrl);
+
+/* USER HISTORY (STATIC FIRST) */
 shortURLRouter.get("/history", protect, getUserUrls);
+
+/* REDIRECT (DYNAMIC LAST) */
+shortURLRouter.get("/:shortcode", redirectFunction);
 
 export default shortURLRouter;
